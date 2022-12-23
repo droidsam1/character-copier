f84951a9842f08f984f1e7fe5ac76f917fd97bed
droidsam1 <droidsam@gmail.com>
2022-12-23 10:58:29 +0100

    add test for copy only one char with mockito

--- src/test/java/com/droidsam/app/CopierWithMockitoTest.java
+++ src/test/java/com/droidsam/app/CopierWithMockitoTest.java
@@ -11,10 +11,26 @@ class CopierWithMockitoTest {
     void shouldNotCopyAnyCharWhenInputIsEmpty() {
         var input = emptySource();
         var output = spy(IDestination.class);
-        
+
         new Copier(input, output).Copy();
 
         verify(output, never()).SetChar(any(char.class));
+    }
+
+    @Test
+    void shouldCopyTheOnlyCharWhenInputHasOnlyOneChar() {
+        var input = oneCharOnly();
+        var output = spy(IDestination.class);
+
+        new Copier(input, output).Copy();
+
+        verify(output, atMostOnce()).SetChar(any(char.class));
+    }
+
+    private ISource oneCharOnly() {
+        var input = mock(ISource.class);
+        when(input.GetChar()).thenReturn('A').thenReturn('\n');
+        return input;
     }
 
     private ISource emptySource() {
